7c1c712c616f7d7ef2df13fd6c592cfc5afdf91d
daishi <daishi@axlight.com>
2018-10-18 14:10:29 +0900

    experimental types definition for ts

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+type mapper<I, O> = (input: I) => O;
+type updater<T> = (f: ((v: T) => T) | T) => void;
+type getterProps<S> =  {[K in keyof S]: S[K]};
+export type composeWithState =
+  <P extends {}, S extends {}>(initialState: S | mapper<P, S>, options?: any) =>
+    React.ComponentType<P & getterProps<S> & any>;
