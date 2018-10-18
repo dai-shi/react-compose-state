7b4b8e2de637746c16416b77026276db829ad0d7
daishi <daishi@axlight.com>
2018-10-18 21:33:28 +0900

    fix type

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,8 +3,8 @@ type Updater<T> = (f: ((v: T) => T) | T) => void;
 type Options = { setters: any };
 type GetterProps<S> =  {[K in keyof S]: S[K]};
 type SetterProps = any;
-export type ComposeWithState =
-  <P extends {}, S extends {}>(initialState: S | Mapper<P, S>, options?: Options) =>
+export type ComposeWithState<P> =
+  <S extends {}>(initialState: S | Mapper<P, S>, options?: Options) =>
     (base: React.ComponentType<P>) =>
       React.ComponentType<P & GetterProps<S> & SetterProps>;
-export const composeWithState: ComposeWithState;
+export const composeWithState: ComposeWithState<any>;
