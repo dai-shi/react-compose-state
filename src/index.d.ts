edf4ff6dc458166125608ed4ae951d526df32dfa
daishi <daishi@axlight.com>
2018-10-18 21:08:29 +0900

    fix and add types entry

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,10 @@
-type mapper<I, O> = (input: I) => O;
-type updater<T> = (f: ((v: T) => T) | T) => void;
-type getterProps<S> =  {[K in keyof S]: S[K]};
-export type composeWithState =
-  <P extends {}, S extends {}>(initialState: S | mapper<P, S>, options?: any) =>
-    React.ComponentType<P> => React.ComponentType<P & getterProps<S> & any>;
+type Mapper<I, O> = (input: I) => O;
+type Updater<T> = (f: ((v: T) => T) | T) => void;
+type Options = { setters: any };
+type GetterProps<S> =  {[K in keyof S]: S[K]};
+type SetterProps = any;
+export type ComposeWithState =
+  <P extends {}, S extends {}>(initialState: S | Mapper<P, S>, options?: Options) =>
+    (base: React.ComponentType<P>) =>
+      React.ComponentType<P & GetterProps<S> & SetterProps>;
+export const composeWithState: ComposeWithState;
