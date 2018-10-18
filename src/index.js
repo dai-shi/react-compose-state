7e5fe611d7046d103c8f416a97a4eb94e5b08352
daishi <daishi@axlight.com>
2018-10-18 22:16:08 +0900

    flow comment type to be compatible with ts

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 // @flow
 
-import React, { type StatelessFunctionalComponent } from 'react';
+import React from 'react';
+/*::
+import type { StatelessFunctionalComponent } from 'react';
+*/
 
 const isFunction = fn => (typeof fn === 'function');
 const capitalize = str => (str.charAt(0).toUpperCase() + str.slice(1));
 
 export const composeWithState = (
-  initialState : (Object | (props: Object) => Object),
-  options: Object = {},
-) => (BaseComponent : StatelessFunctionalComponent<any>) => (
-  class extends React.PureComponent<any, any> {
-    constructor(props: Object) {
+  initialState /*: (Object | (props: Object) => Object)*/,
+  options /*: Object */ = {},
+) => (BaseComponent /*: StatelessFunctionalComponent<any> */) => (
+  class extends React.PureComponent/*:: <any, any>*/ {
+    constructor(props /*: Object */) {
       super(props);
       if (isFunction(initialState)) {
-        const initializeState = (initialState: (props: Object) => Object);
+        const initializeState = (initialState /*: (props: Object) => Object */);
         this.state = initializeState(props);
       } else {
         this.state = initialState;
@@ -31,9 +34,11 @@ export const composeWithState = (
       });
     }
 
+    /*::
     stateSetters: {
       [string]: string => void,
     };
+    */
 
     render() {
       return (
